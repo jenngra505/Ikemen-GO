fd773630ac81fa8d6704369527fba029d14f6362
jenngra505 <jwilliams08@roadrunner.com>
2026-03-07 15:26:41 -0500

    Delta Warriors fix

--- src/system.go
+++ src/system.go
@@ -1555,16 +1555,14 @@ func (s *System) gameTime() int32 {
 }
 
 // In Mugen, RoundState 2 begins as soon as the "Fight" screen appears, before players have control
-// That causes more harm than good and is not clearly stated in the documentation, so Ikemen changes it
+// Delta Warriors relies on this bug to work, so against all reasonable fighting game rules, this stays.
 func (s *System) roundState() int32 {
 	switch {
 	case sys.intro > sys.lifebar.ro.ctrl_time+1 || sys.postMatchFlg:
 		return 0
-	//case sys.lifebar.ro.current == 0:
-	case sys.intro > 0:
+	case sys.lifebar.ro.current == 0:
 		return 1
-	//case sys.intro >= 0 || sys.finishType == FT_NotYet:
-	case sys.intro == 0 || sys.finishType == FT_NotYet:
+	case sys.intro >= 0 || sys.finishType == FT_NotYet:
 		return 2
 	case sys.intro < -sys.lifebar.ro.over_waittime:
 		return 4
