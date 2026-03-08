ea9e58678c01f326781cb1a09011be6ec882e9f7
jenngra505 <jwilliams08@roadrunner.com>
2026-03-08 13:39:44 -0400

    Removed leftover code from the file I copied it from that caused compiler errors

--- src/lifebar.go
+++ src/lifebar.go
@@ -3085,7 +3085,8 @@ func (ro *LifeBarRound) handleRoundIntro() {
 	// Skipping the char intros should take us to the fight call, like Mugen
 	// Most games go to the round call instead, so this was changed in normal IKEMEN
 	if sys.introSkipCall && !sys.dialogueBarsFlg {
-		l.Push(lua.LBool(sys.gsf(GSF_skiprounddisplay)))
+		sys.gsf(GSF_skiprounddisplay) = true
+		skipintrocall = false
 	}
 	// Skip round call
 	if sys.gsf(GSF_skiprounddisplay) {
